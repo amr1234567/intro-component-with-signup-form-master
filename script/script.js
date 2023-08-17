f792b5d1164698a7e8918b799f4f03cf656889d1
Amr Shalaby <53496816+amr1234567@users.noreply.github.com>
2023-08-17 15:43:45 +0300

    desktop version

--- script/script.js
+++ script/script.js
@@ -1,24 +1,48 @@
-let numbers = document.querySelectorAll('.rating div');
-let value = document.querySelector('clicked-number');
+"use strict";
 
-numbers.forEach((element, index, arr) => {
-    element.addEventListener('click', () => {
-        arr.forEach((ele) => {
-            ele.classList.replace('clicked-number', 'number');
-        });
-        element.classList.replace('number', 'clicked-number');
-    })
-})
+let inputFields = document.querySelectorAll('.input-field input');
+let marks = document.querySelectorAll('.input-field div')
+let spans = document.querySelectorAll('.input-field span');
+let mailformat = /\w+@\w+.\w+/;
+let confirm = true;
 
 function validate() {
-    let value = document.querySelector('.clicked-number');
-    if (value == null) {
-        alert("please choose the rate");
-        return false;
-    }else{
-        localStorage.setItem('myValue', value.textContent);
-        return true;
-    }
+    inputFields.forEach((ele, index) => {
+        if (ele.value === '') {
+            spans[index].textContent = `${ele.getAttribute('name')} cannot be empty`;
+            ele.classList.add('input-invalid');
+            marks[index].classList.replace('disabled-mark', 'mark');
+            confirm = false;
+        } else if (ele.getAttribute('name') === 'Email') {
+            if (!ele.value.match(mailformat)) {
+                marks[index].classList.replace('disabled-mark', 'mark');
+                ele.classList.add('input-invalid');
+                spans[index].textContent = `Looks like this is not an ${ele.getAttribute('name')}`;
+                confirm = false;
+            } else {
+                ele.classList.remove('input-invalid');
+                spans[index].textContent = '';
+                confirm = true;
+                marks[index].classList.replace('mark', 'disabled-mark');
+            }
+        } else if (ele.getAttribute('name') === 'Password') {
+            if (ele.value.length < 8) {
+                marks[index].classList.replace('disabled-mark', 'mark');
+                ele.classList.add('input-invalid');
+                spans[index].textContent = `The ${ele.getAttribute('name')} must be bigger than 8 chars`;
+                confirm = false;
+            } else {
+                ele.classList.remove('input-invalid');
+                spans[index].textContent = '';
+                confirm = true;
+                marks[index].classList.replace('mark', 'disabled-mark');
+            }
+        } else {
+            ele.classList.remove('input-invalid');
+            spans[index].textContent = '';
+            confirm = true;
+            marks[index].classList.replace('mark', 'disabled-mark');
+        }
+    });
+    return confirm;
 }
-
-
